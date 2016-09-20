cceaf4dc79579f0e77a840949a3046ba80d5b3af
HAN <hsh8616@naver.com>
2016-09-20 15:26:53 +0900

    Initial Commit

---
+++ MediNeye/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MediNeye
+//
+//  Created by HAN on 2016. 9. 20..
+//  Copyright © 2016년 HAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MediNeye/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MediNeye
+//
+//  Created by HAN on 2016. 9. 20..
+//  Copyright © 2016년 HAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
